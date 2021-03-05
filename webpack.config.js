50c125cd85c5bd21aa6534f7f5138185c752704a
ConnorMul <mulhollandconnor@icloud.com>
2021-03-05 14:23:28 -0500

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const webpack = require('webpack');
+
+
+
+
+/*
+ * We've enabled MiniCssExtractPlugin for you. This allows your app to
+ * use css modules that will be moved into a separate CSS file instead of inside
+ * one of your module entries!
+ *
+ * https://github.com/webpack-contrib/mini-css-extract-plugin
+ *
+ */
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+
+
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+
+
+
+
+module.exports = {
+  mode: 'development',
+  entry: './index.html.js',
+
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({ filename:'main.[contenthash].css' }),
+    new WorkboxWebpackPlugin.GenerateSW({
+            swDest: 'sw.js',
+            clientsClaim: true,
+            skipWaiting: false,
+          })
+  ],
+
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      include: [],
+      loader: 'babel-loader'
+    }, {
+      test: /.css$/,
+
+      use: [{
+        loader: MiniCssExtractPlugin.loader
+      }, {
+        loader: "css-loader",
+
+        options: {
+          sourceMap: true
+        }
+      }]
+    }]
+  },
+
+  devServer: {
+    open: true,
+    host: 'localhost'
+  }
+}
